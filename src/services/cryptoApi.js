1660930563b9ea94f237d18d099da23f5ef20146
Venkatesh Ananthoju <49225539+venky27011995@users.noreply.github.com>
2022-02-03 13:27:34 +0530

    updated API

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,8 +2,10 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 
 const cryptoApiHeader = {
+    //'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    //'x-rapidapi-key': '2d63abdf94msh134964cab96febfp1875d3jsnbc4e7a5fe904',
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '2d63abdf94msh134964cab96febfp1875d3jsnbc4e7a5fe904'
+    'x-rapidapi-key': '47fb3137e1msh3d268f1113aa3b1p1d44c6jsnf9071929ff05'
 }
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 const createRequest = (url) =>({url, headers:cryptoApiHeader});
